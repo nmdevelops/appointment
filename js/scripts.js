8e32d487ce2d97d7a7f2e3687c804c4a713c222e
Nathan Mayer <nm.develops@gmail.com>
2017-05-31 16:24:34 -0700

    Add userEmail and userTel fields to form as well as check box for opt in text reminder and indentation clean up

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var userFirstNameInput = $("input#userFirstName").val();
     var userLastNameInput = $("input#userLastName").val();
+    var userEmailInput = $("input#userEmail").val();
+    var userTelInput = $("input#userTel").val();
     var appointmentDateInput = $("input#appointmentDate").val();
     var appointmentStartTimeInput = $("select#appointmentStartTime").val();
     var appointmentEndTimeInput = $("select#appointmentEndTime").val();
@@ -11,6 +13,8 @@ $(document).ready(function() {
 
     $(".userFirstName").text(userFirstNameInput);
     $(".userLastName").text(userLastNameInput);
+    $(".userEmail").text(userEmailInput);
+    $(".userTel").text(userTelInput);
     $(".appointmentDate").text(appointmentDateInput);
     $(".appointmentStartTime").text(appointmentStartTimeInput);
     $(".appointmentEndTime").text(appointmentEndTimeInput);
